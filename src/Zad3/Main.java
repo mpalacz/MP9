dea72ee5ec87d58d2a3ed3147470526b80b6eb98
DesktopPC <mpalacz00@gmail.com>
2021-12-10 11:43:18 +0100

    Zad3

--- src/Zad3/Main.java
+++ src/Zad3/Main.java
@@ -1,17 +1,23 @@
-package Zad2;
+package Zad3;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
-public class ShowFile {
+public class Main {
     public static void main(String[] args) {
         int bajt;
 
         try (FileInputStream fis = new FileInputStream("plik.txt")) {
+            FileOutputStream fos = new FileOutputStream("output.txt");
             do {
                 bajt = fis.read();
-                if (bajt != -1) System.out.println((char) bajt);
+                if (bajt != -1) {
+                    if (bajt == ' ')
+                        bajt = '-';
+                    fos.write(bajt);
+                }
             } while (bajt != -1);
         } catch (FileNotFoundException e) {
             System.out.println("Nie znaleziono pliku");
@@ -19,5 +25,6 @@ public class ShowFile {
         } catch (IOException e) {
             System.out.println("Błąd odczytu pliku");
         }
+
     }
 }
