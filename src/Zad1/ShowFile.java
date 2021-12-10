20c120ae24a7051f8bf3b7d44872c4a3072ba096
DesktopPC <mpalacz00@gmail.com>
2021-12-10 10:28:05 +0100

    Initial commit

---
+++ src/Zad1/ShowFile.java
@@ -0,0 +1,29 @@
+package Zad1;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class ShowFile {
+    public static void main(String[] args) throws IOException {
+        FileInputStream fis;
+        int bajt;
+
+        try {
+            fis = new FileInputStream("C:\\Users\\mpala\\Desktop\\plik.txt");
+        } catch (FileNotFoundException e) {
+            System.out.println("Nie znaleziono pliku");
+            return;
+        }
+        try {
+            do {
+                bajt = fis.read();
+                if (bajt != -1) System.out.println((char) bajt);
+            } while (bajt != -1);
+        } catch (IOException e) {
+            System.out.println("Błąd odczytu pliku");
+        } finally {
+            fis.close();
+        }
+    }
+}
